ba127557d6b42806463a31eb71715655f776d09c
KasunHasanga <kasunhasanga96@gmail.com>
2021-12-19 12:36:27 +0530

    collection card bacis layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
-import './App.css';
-import Header from './component/Header';
+import "./App.css";
+import CollectionCard from "./component/CollectionCard";
+import Header from "./component/Header";
 
 function App() {
-  return  <div className='app'>
-      <Header/>
-  </div>
-  
+  return (
+    <div className="app">
+      <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana punk"}
+        traits={[{ value: 7 }]}
+        image="https://lh3.googleusercontent.com/p_oi7tfWbom8CyUnj4v-gJGkzeaAOjGyfgfojnhZusszQ1blEAEIVm9mlEIqCHJif52D7XVyJrqXb89l7NrmdMWPmxA1gDraCAXgIA=w600"
+      />
+    </div>
+  );
 }
 
 export default App;
